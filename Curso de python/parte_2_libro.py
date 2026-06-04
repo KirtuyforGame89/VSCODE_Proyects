0796c49381e7282711e06ad46c0dce99424ef0a2
KirtuyforGame89 <mantilladiazronaldo@gmail.com>
2026-06-04 01:32:43 -0500

    reestructura de las variables

--- Curso de python/parte_2_libro.py
+++ Curso de python/parte_2_libro.py
@@ -1,11 +1,14 @@
 #números ææææææ ¢¢„»»»«««»«»«»«»«xzxzxxz+alt_gr
 #enteros
-print(2+3*4)
-print(2.5*4)
-print(0.1+0.1)
-print(0.7+3.4)
-print(0.2+0.1)
-print(3*0.1 + "pasword")
+se = 2+3*4
+re = 2.5*4
+rt = 0.1+0.1
+fd = 0.7+3.4
+fg = 0.2+0.1
+gy, mark = 3*0.1, "pasword"
 #utilizar esto para listas 
 
-
+mork = 3
+merk = 'kirtuy'
+print(f'!!Hola {merk}, se te a asignado n°{mork}')
+print('Facilito no?')
